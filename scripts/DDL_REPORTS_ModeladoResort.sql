3928c8cd9a9838d2efaf6a6be8db08c88a967998
Cristian Cardona <1007220881@u.icesi.edu.co>
2023-11-16 12:18:46 -0500

    add script for resort interest reports

---
+++ scripts/DDL_REPORTS_ModeladoResort.sql
@@ -0,0 +1,18 @@
+-- Reportes de interes para el resort 
+
+
+/*
+Reporte de Ocupación: Muestra la ocupación diaria, semanal y mensual de las habitaciones
+y actividades.
+*/
+
+
+/*
+Reporte de Ingresos: Genera un informe de ingresos diarios, semanales y mensuales
+desglosado por habitaciones, actividades y servicios adicionales.
+*/
+
+/*
+Reporte de Satisfacción del Cliente: Basado en las encuestas de satisfacción recopiladas al
+momento del check-out.
+*/
